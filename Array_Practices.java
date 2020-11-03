7b5f1a2d45d7396b950f9928214e6c8f8035ebe2
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-03 15:00:11 +0500

    Add files via upload

---
+++ Array_Practices.java
@@ -0,0 +1,34 @@
+package com.company;
+class Array_Practices
+{
+    public static void main(String[] args)
+    {
+       /* int[] value = {112, 213, 341, 412};
+        System.out.println(name[1]);
+*/
+
+      /*  String[] name = {"Waheed","Horair","Waqas" ,"Mabtoor"};
+        for(int i=0;i<=name.length; i++)
+        {
+            System.out.println(name[i]);
+        }*/
+
+
+      /*  double[] values = new double[4];
+        values[0]= 2.21;
+        values[1] = 61.3;
+        values[2] =21.3;
+        values[3] =12.6;
+        System.out.println(values[1]);*/
+
+
+       int [] no = {21,31,12,123,32,32,43,45,65,34,12,43,654,7,5,233,1213,435,65,75,233,12,13,465,75766,7564,53};
+        for (int elements:
+             no) {
+            System.out.println(elements);
+
+        }
+
+
+    }
+}
