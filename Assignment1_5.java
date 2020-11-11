5be7e80bbff4ea465491e014dd0a48cdbb7e1bd0
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-11 12:36:05 +0500

    Add files via upload

---
+++ Assignment1_5.java
@@ -0,0 +1,470 @@
+package com.company;
+import java.util.Scanner;
+public  class  Assignment1_5
+{
+    public static void main(String[] args)
+    {
+           /* int a = 10;
+            int b = 20;
+            int c = a+b;
+        System.out.println(c);*/
+
+           /* float a = 65;
+            float b = 85;
+            float c = 95;
+            float d = 55;
+            float e = (a+b+c+d)/500*100;
+            System.out.println(e);*/
+
+        Scanner sc = new Scanner(System.in);
+
+
+        /*String name = sc.nextLine();
+        System.out.println("Hello " + " " +  name + " " + " Have A Nice Day");*/
+
+
+        /*System.out.println("Enter The value");
+        float no = sc.nextFloat();
+        float m = no * 1000;
+        System.out.println("The Value In Meter is :" + m);*/
+
+
+    /*    System.out.println(sc.hasNextInt());*/
+
+
+       /* float a = 7/4 * 9/2;
+        System.out.println(a);*/
+
+
+      /* int a = 20;
+       int user = sc.nextInt();
+        System.out.println(a>user);*/
+
+
+        /*double a = 10;
+        double s = 20;
+        double v = 31;
+        double u = 43;
+        double e = (v*v - u*u)/2*a*s;
+        System.out.println(e);
+*/
+
+
+        /*int a =7 + 49/7 + 35/7;
+        System.out.println(a);*/
+
+
+                /*System.out.println("Enter Your Name");
+                String lower = sc.nextLine();
+                String lowe = lower.toLowerCase();
+                System.out.println(lowe);*/
+
+
+           /* String word = "This  Is  The World  Of String  Online";
+            String under = word.replace("  ", "_");
+        System.out.println(under);*/
+
+
+
+
+       /* String name = sc.nextLine();
+        String letter = "Dear  <|NAME|> ! Thanks A lot";
+        String rep = letter.replace("<|NAME|>" ,name );
+        System.out.println(rep);*/
+
+
+
+
+
+      /* int a = 10;
+       if (a==11)
+       {
+           System.out.println("I am 11");
+       }
+       else
+       {
+           System.out.println("I am Not 11");
+       }*/
+
+
+
+        /*float no1 = sc.nextFloat();
+        float no2 = sc.nextFloat();
+        float no3 = sc.nextFloat();
+        float no = (no1+no2+no3)/300 *100;
+        if (no >= 40 && no1 >=33 && no2>=33 & no>=33)
+        {
+            System.out.println("You Are Pass");
+        }
+
+        else
+        {
+            System.out.println("You Are Fail");
+        }*/
+
+
+
+           /* float no = sc.nextFloat();
+            if(no>=2.5f && no<=5.0f)
+            {
+                no  =  2.5f - 5/100;
+            }
+            else if(no>=5.0f && no<=10.0f)
+            {
+                no = 20% - 5.0f;
+            }
+            else {
+                no = 30% -10.0f;
+            }
+
+        System.out.println(no);*/
+
+
+
+
+        int no = sc.nextInt();
+        if(no == 1)
+        {
+            System.out.println("Monday");
+        }
+        else if(no == 2)
+        {
+            System.out.println("Tuesday");
+        }
+        else if(no == 3)
+        {
+            System.out.println("Wednesday");
+        }
+        else if(no == 4)
+        {
+            System.out.println("Thursday");
+        }
+        else if(no == 5)
+        {
+            System.out.println("Friday");
+        }
+        else if(no == 6)
+        {
+            System.out.println("Saturday");
+        }
+        else if(no == 7)
+        {
+            System.out.println("Sunday");
+        }
+
+
+/*
+
+        int year = sc.nextInt();
+        if(year%4==0)
+        {
+            System.out.println("This is Leap Year");
+        }
+        else
+        {
+            System.out.println("This is not a Leap Year");
+        }
+*/
+
+
+        /*System.out.println("Enter Website URL Extension");
+        String  url = sc.nextLine();
+        if(url.equals(".com") )
+        {
+            System.out.println("This Is Commercial Website");
+        }
+        else if(url.equals(".org"))
+        {
+            System.out.println("This Is Organiztaion Website");
+        }
+        else if (url.equals(".in"))
+        {
+            System.out.println("This Is Indian Website");
+        }*/
+
+
+
+
+
+        /*int no = 4;
+        for(int i=no;i>0;i--)
+        {
+           for(int j=0;j<i;j++)
+           {
+               System.out.print("*");
+           }
+            System.out.print("\n");
+        }*/
+
+
+
+
+
+        /*int no =3;
+        int sum = 0;
+        int i =0;
+        while(i<no)
+        {
+            sum = sum +(2*i);
+           i++;
+        }
+
+        System.out.println(sum);
+*/
+
+
+
+      /*  int n = 5;
+        int i = 0;
+        for (i=0;i<=10;i++)
+        {
+            System.out.println(n + "  x" + " " +  i +  "  = " + " " + n*i);
+        }*/
+
+
+
+       /* int n = 6;
+        for(int i=10 ; i>=0; i--)
+        {
+            System.out.println(n + " x " + " " +i + " = " + " " + n*i);
+        }*/
+
+
+/*
+                int n = 3;
+                int fac = 1;
+                for(int i=1;i<=n;i++)
+                {
+                    fac*=i;
+                }
+        System.out.println(fac);*/
+
+
+
+       /* int n = 5;
+        int fac = 1;
+        int i = 1;
+        while(i<=n)
+        {
+            fac*=i;
+            i++;
+        }
+        System.out.println(fac);*/
+
+
+
+
+
+        /*int no =8;
+        int sum =0;
+        for(int i=0;i<no;i++)
+        {
+            sum = sum + (2*i);
+        }
+        System.out.println(sum);
+    }*/
+
+
+
+      /*  float [] arr = {31.4f,42.13f,23.24f,234.1f,23,54f};
+        float sum =0;
+        for(float ele: arr)
+        {
+            sum = sum+ ele;
+        }
+        System.out.println(sum);*/
+
+
+
+
+
+
+
+
+
+//        int no = 4;
+//        int sum = 0;
+//        for (int i = no; i>0;i--)
+//        {
+//            for (int j=0;j<i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+
+
+
+
+
+        /*
+        int no =3;
+        int sum = 0;
+        int i =0;
+        while(i<no)
+        {
+            sum = sum + (2*i);
+            i++;
+        }
+        System.out.println(sum);
+*/
+
+       /* int n = 4;
+        int fac = 1;
+        for (int i=1;i<=n;i++)
+        {
+            fac*=i;
+        }
+        System.out.println(fac);*/
+
+
+
+
+
+        /*float [] arr = {54.8f,90,9f,87.8f,98.95f,09.76f};
+        float sum = 0;
+        for (float ele : arr)
+        {
+            sum = sum + ele;
+        }
+        System.out.println(sum);
+*/
+
+
+
+       /* int [] arr = {21,2,12,1,2,1,2,13,13,23,23,12,1,21};
+        int no = 21;
+        boolean isArray = false;
+        for (int ele: arr)
+        {
+            if(no == ele)
+            {
+                isArray = true;
+            }
+        }
+         if(isArray)
+         {
+             System.out.println("Element is Present In Array");
+         }
+         else
+         {
+             System.out.println("Element is Not Present In Array");
+         }*/
+
+
+/*
+
+            float [] arr = {54.8f,90,9f,87.8f,98.95f,09.76f};
+            float sum = 0;
+            for(float ele: arr)
+            {
+                sum = sum + ele;
+            }
+        System.out.println("The Average Of Number is " + sum/ arr.length);
+*/
+
+
+
+
+      /*  int [][] arr1= {{0,1,0},{0,1,1}};
+        int [][] arr2 = {{0,0,1}, {0,1,1}};
+        int [] [] res = {{0,0,0},{0,0,0}};
+        for (int i =0;i<arr1.length;i++)
+        {
+            for (int j=0; j<arr1[i].length;j++)
+            {
+                res[i][j] = arr1[i][j]+arr2[i][j];
+                System.out.print(res[i][j]);
+            }
+            System.out.println("\n");
+        }*/
+
+
+
+
+/*int [] arr = {10,20,30,40,50,60};
+int l = arr.length;
+int no = Math.floorDiv(l,2);
+for (int i=0;i<no;i++)
+{
+    int temp= arr[i];
+    arr[i] = arr[l-1-i];
+    arr[l-1-i] = temp;
+}
+for (int ele : arr)
+{
+    System.out.println(ele);
+}*/
+
+
+
+
+       /*int [] arr = {21,21,21,2,12,3,431,32,21,3};
+       int no = Integer.MIN_VALUE;
+            for(int ele : arr)
+            {
+                if(no<ele)
+                {
+                    no = ele;
+                }
+            }
+        System.out.println(no);*/
+
+
+
+      /*
+       int [] arr = {12,1,21,31,31,31,43,11};
+        int no =0;
+        boolean isSorted = true;
+        for (int i=0; i<arr.length-1;i++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+             isSorted = false;
+            }
+        }
+
+//        if(isSorted)
+//{
+//    System.out.println("The Value is Sorted");
+//}
+//else
+//{
+//    System.out.println("Value iS not Sorted");
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
