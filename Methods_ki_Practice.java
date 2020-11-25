cd137390e7b58dfb4ec07cf82ec4385edc101d16
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-25 09:43:47 +0500

    Add files via upload

---
+++ Methods_ki_Practice.java
@@ -0,0 +1,89 @@
+package com.company;
+
+public  class  Methods_ki_Practice
+{
+//    static  int  Add(int x, int y )
+//    {
+//        int z;
+//        z = x + y;
+//        return z;
+//    }
+
+
+//      int  Add(int x, int y )
+//    {
+//        int z;
+//        z = x + y;
+//        return z;
+//    }
+
+//    static  int Add (int x)
+//    {
+//    int y = x +x;
+//    return y;
+//    }
+//
+//    static int Add(int x, int y)
+//    {
+//        int z = x+y;
+//        return z;
+//    }
+//    static  int Add (int x,int y, int z)
+//    {
+//        int d = x+y+z;
+//        return d;
+//    }
+
+
+
+//
+//    static  int Sum (int ... arr)
+//    {
+//        int result = 0;
+//        for (int ele : arr)
+//        {
+//            result+=ele;
+//        }
+//        return  result;
+//    }
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 43;
+//        int c = Add(a,b);
+//        System.out.println(c);
+
+
+//        int a = 20;
+//        int b = 54;
+//        Methods_ki_Practice obj = new Methods_ki_Practice();
+//        int c = obj.Add(a,b);
+//        System.out.println(c);
+//    }
+
+//        int a = 10;
+//        int b = 43;
+//        int i = 10;
+//        int c = Add(a);
+//        int d = Add(a,b);
+//        int e = Add(a,b,i);
+//
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+
+//
+//        System.out.println(Sum(21,32,45,65,10));
+
+    }
+}
