cae2107a75cf2790743e8046f205984180f2880d
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-12-21 10:46:29 +0500

    Add files via upload
    Packages Work Done

---
+++ Java Custom Packages/problem_1.java
@@ -0,0 +1,45 @@
+package  calc;
+
+class  Calculator
+{
+    public void Add(int n1,int n2)
+    {
+        System.out.println("I am Simple Calculator");
+    }
+    
+    
+ }
+
+ class  HyCalculator
+{
+    public void Add(int n1,int n2)
+    {
+        System.out.println("I am HYbrid Calculator");
+    }
+    
+    
+ }
+
+
+class  SCICalculator
+{
+    public void Add(int n1,int n2)
+    {
+        System.out.println("I am Scientific Calculator");
+    }
+    
+    
+ }
+
+
+
+public class problem_1
+ {
+
+
+public static void main(String[] args)
+{
+    System.out.println(" I am A Calculator");
+}
+
+ }
---
+++ Java Custom Packages/problem_2.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class problem_2
+{
+
+public static void main(String[] args)
+{
+
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+System.out.println("Hello Mr" + a);
+
+
+} 
+
+
+}
